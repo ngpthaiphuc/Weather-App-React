9924efed95ecd6eb0d9ed9a072f98855f0a37e31
Thai <ngpthaiphuc@gmail.com>
2021-04-24 16:25:45 -0400

    [ADD] Fetching from weather API and display info from a zip code or from the user location

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import API_KEY from './keys';
+import { Input, Button } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    //const API_KEY = process.env.REACT_APP_api_key;
+    const [weather, setWeather] = useState(null);
+    const [zipCode, setZipCode] = useState(22904);
+    //const [lat, setLat] = useState();
+    //const [long, setLong] = useState();
+
+    /*useEffect(() => {
+        const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+        url.searchParams.append("appid", "2b553aeab9a111b4d435be3726d191c1");
+        url.searchParams.append("zip", 22904); //Charlottesville
+        url.searchParams.append("units", "imperial"); //Fahrenheit
+        fetch(url)
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((obj) => {
+                //if (obj.cod === 200) { //no html errors
+                    setWeather(obj);
+                //} else {
+                    //setWeather(false);
+                //}
+            });
+    }, []);*/
+    //console.log(API_KEY);
+
+    const fetchWeather = () => {
+        const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+        url.searchParams.append("appid", API_KEY);
+        url.searchParams.append("zip", zipCode); //default: Charlottesville
+        url.searchParams.append("units", "imperial"); //Fahrenheit
+        fetch(url)
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((obj) => {
+                if (obj.cod === 200) { //no html errors
+                    setWeather(obj);
+                } else {
+                    setWeather(false);
+                }
+            });
+    }
+
+    const fetchOneCallWeather = (position) => {
+        const url = new URL("https://api.openweathermap.org/data/2.5/onecall");
+        url.searchParams.append("appid", API_KEY);
+        console.log(position.coords.latitude + ", " + position.coords.longitude);
+        url.searchParams.append("lat", position.coords.latitude);
+        url.searchParams.append("lon", position.coords.longitude);
+        url.searchParams.append("units", "imperial"); //Fahrenheit
+        fetch(url)
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((obj) => {
+                //if (obj.cod === 200) { //no html errors
+                    setWeather(obj);
+                /*} else {
+                    setWeather(false);
+                }*/
+            });
+
+        //console.log(weather);
+    }
+
+    function getUserLocation(){
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(fetchOneCallWeather);
+        } else {
+            console.log("Geolocation is not supported");
+        }
+    }
+
+    if (weather === null) {
+        return (
+            <div>
+                <Input type="number" value={zipCode} onChange={(event) => {
+                    setZipCode(event.target.value);
+                }} />
+                <Button
+                    variant="contained"
+                    onClick={fetchWeather}
+                >Zip Code</Button>
+                <Button
+                    variant="contained"
+                    onClick={getUserLocation}
+                >User Location</Button>
+            </div>
+        );
+    }
+
+    console.log(weather);
+
+    return (
+        <div style={{ textAlign: "center" }}>
+
+            <pre>{JSON.stringify(weather,undefined,4)}</pre>
+        </div>
+    );
 }
-
-export default App;
---
+++ src/WeatherDisplay.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import { Input, Button } from '@material-ui/core';
+
+export default function WeatherDisplay(weather) {
+
+    return (
+        <div>
+
+        </div>
+    );
+
+
+}
